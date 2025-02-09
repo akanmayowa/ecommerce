c3f6df5255bd7324451ea5615c93c5ce479543e1
akanmayowa <akanmayowa@yahoo.com>
2025-02-09 12:04:52 +0000

    fix issues with user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,22 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    public const USER = "user";
+    public const SUPPLIER = "supplier";
+
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
-     *
      * @var list<string>
      */
     protected $fillable = [
@@ -45,5 +48,45 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    /**
+     * check if auth user is shipper
+     * @return bool
+    */
+    public static function isSupplier(): bool
+    {
+        return auth()->check()
+            && auth()->user()->role == 'supplier';
+    }
+
+
+    /**
+     * check if auth user is a shopper
+     * @return bool
+    */
+    public static function isUser(): bool
+    {
+        return auth()->check() &&
+            auth()->user()->role == 'user';
+    }
+
+    /**
+     * Get the products added by the supplier.
+     * @return HasMany
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'supplier_id');
+    }
+
+    /**
+     *the relationship to orders where the user is the customer
+     * @return HasMany
+    */
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 }
