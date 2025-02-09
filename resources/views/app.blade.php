bcfffdc46b3664893982bc091a2dba39ce7b92ca
akanmayowa <akanmayowa@yahoo.com>
2025-02-09 11:58:13 +0000

    modify app.blade.php abd welcome.jsx files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,20 +3,21 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
-        @inertiaHead
     </head>
     <body class="font-sans antialiased">
-        @inertia
+        <div class="min-h-full">
+            @include('layouts.navbar')
+            <main>
+                <div id="app" class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    @yield('content')
+                </div>
+            </main>
+            @include('layouts.footer')
+        </div>
     </body>
 </html>
